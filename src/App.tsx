4c5fb9aa9225f9d5731ed48bf7be5fbdf6a55a77
varditomer <tomer.vardi@gmail.com>
2023-07-03 14:14:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <section className="main-app">
+      App works!
+    </section>
+  )
+
+}
+
+export default App
