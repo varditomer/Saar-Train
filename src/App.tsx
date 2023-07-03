c53e4141ab77645ce1f66a2d152feff0700b9f10
varditomer <tomer.vardi@gmail.com>
2023-07-03 17:05:59 +0300

    btn style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './assets/styles/main.scss'
+import { Home } from './pages/Home'
 
 function App() {
   return (
     <section className="main-app">
-      App works!
+      <Home />
     </section>
   )
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export const Home: React.FC = () => {
+
+    return (
+        <section className="home">
+            {/* <h1>Welcome to Saar Soccer Training</h1> */}
+            <img src="src\assets\logo\argus_logo-removebg.png" alt="" />
+            <button className="select-train button">Enter</button>
+        </section>
+    )
+
+}
